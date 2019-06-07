3137cba8afc4a58c42234eda14d7b544d4dcb1c0
murillo9070 <49643525+murillo9070@users.noreply.github.com>
2019-06-07 09:13:18 -0300

    resolvendo o erro de JS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const ativa = document.getElementById ("btn-ativa")
-const containermenu = document.getElementById("mobile-container")
-const menudesktop = document.getElementById ("menu-desktop")
+const ativa = document.getElementById("btn-ativa");
+const containermenu = document.getElementById("mobile-container");
+const menudesktop = document.getElementById("menu-desktop");
 
-let contador = 0
+let contador = 0;
 
 ativa.addEventListener('click', (e) => {
-    containermenu.classList.remove ("_hiddentop")
-    containermenu.classList.add ("_activemeu")
-    menudesktop.classList.add ("color-mobile")
-    contador++
+    containermenu.classList.remove("_hiddentop");
+    containermenu.classList.add("_activemenu");
+    menudesktop.classList.add("color-mobile");
+    contador++;
     if (contador == 2) {
-        containermenu.classList.remove ("_hiddentop")
-        containermenu.classList.add ("_activemeu")
-        menudesktop.classList.add ("color-mobile")
-        contador = 0
+        containermenu.classList.remove("_activemenu");
+        menudesktop.classList.remove("color-mobile");
+        containermenu.classList.add("_hiddentop");
+        contador = 0;
     }
-})
+});
