b5fb01704a05fe04a2dbcf792cd91150d6e91939
murillo9070 <49643525+murillo9070@users.noreply.github.com>
2019-08-20 08:45:51 -0300

    mudando geral

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-const ativa = document.getElementById("btn-ativa")
-const containermenu = document.getElementById("mobile-container")
-const menudesktop = document.getElementById("menu-desktop")
-
-let contador = 0
-
-ativa.addEventListener('click', (e) => {
-    containermenu.classList.remove("_hiddentop")
-    containermenu.classList.add("_activemenu")
-    menudesktop.classList.add("color-mobile")
-    contador++
-    if (contador == 2) {
-        containermenu.classList.remove("_activemenu")
-        menudesktop.classList.remove("color-mobile")
-        containermenu.classList.add("_hiddentop")
-        contador = 0
-    }
-})
-
 // const ativa = document.getElementById("btn-ativa")
 // const containermenu = document.getElementById("mobile-container")
 // const menudesktop = document.getElementById("menu-desktop")
 
+// let contador = 0
+
 // ativa.addEventListener('click', (e) => {
-//     animemenu.classList.toggle("_activemenu")
-//     containermenu.classList.toggle("_activemenu")
-//     menudesktop.classList.toggle("color-mobile")
-// }) 
+//     containermenu.classList.remove("_hiddentop")
+//     containermenu.classList.add("_activemenu")
+//     menudesktop.classList.add("color-mobile")
+//     contador++
+//     if (contador == 2) {
+//         containermenu.classList.remove("_activemenu")
+//         menudesktop.classList.remove("color-mobile")
+//         containermenu.classList.add("_hiddentop")
+//         contador = 0
+//     }
+// })
+
+const ativa = document.getElementById("btn-ativa")
+const containermenu = document.getElementById("mobile-container")
+const menudesktop = document.getElementById("menu-desktop")
+
+ativa.addEventListener('click', (e) => {
+    animemenu.classList.toggle("_activemenu")
+    containermenu.classList.toggle("_activemenu")
+    menudesktop.classList.toggle("color-mobile")
+}) 
